d772bb975a2512513696fdc5fb02b28708eb9506
ryyhu <ryanhu021@gmail.com>
2021-05-18 22:49:23 -0700

    Setup views and routes

---
+++ models/person.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const personSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	}
+});
+
+module.exports = mongoose.model('Book', bookSchema);
