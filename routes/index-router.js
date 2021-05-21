4320bc474044ae622efbdc2b17841f9ab41d9d57
ryyhu <ryanhu021@gmail.com>
2021-05-21 13:03:36 -0700

    Add, edit, delete user

--- routes/index-router.js
+++ routes/index-router.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.render('index');
+	if (req.isAuthenticated()) {
+		res.render('index', { user: req.user, auth: req.isAuthenticated() });
+	} else {
+		res.render('index', { user: null, auth: req.isAuthenticated() });
+	}
 });
 
 module.exports = router;
